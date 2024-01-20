742b634b4758fe3200c9d880c598a76c656a44bd
Juan Morais <timrodz@icloud.com>
2024-01-19 23:30:51 -0500

    refactor: Styling glitch for hovering over tournaments

--- lib/mtg_friends_web/components/core_components.ex
+++ lib/mtg_friends_web/components/core_components.ex
@@ -526,7 +526,7 @@ defmodule MtgFriendsWeb.CoreComponents do
               </div>
             </td>
             <td :if={@action != []} class="relative w-14 p-0">
-              <div class="relative whitespace-nowrap py-4 text-right text-sm font-medium">
+              <div class="whitespace-nowrap py-4 text-right text-sm font-medium">
                 <span class="absolute -inset-y-px -right-4 left-0 group-hover:bg-zinc-50 sm:rounded-r-xl" />
                 <span
                   :for={action <- @action}
