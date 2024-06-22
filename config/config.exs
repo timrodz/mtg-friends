963cbf8321398eb69d3a130c218adf06f562dfa2
Juan Morais <timrodz@icloud.com>
2024-06-23 01:05:16 +1200

    fix: CSS nesting Either tailwind and/or ESBuild don't support very deep nesting rule—The results are nested rules being applied to the parent object

--- config/config.exs
+++ config/config.exs
@@ -31,10 +31,10 @@ config :mtg_friends, MtgFriends.Mailer, adapter: Swoosh.Adapters.Local
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.17.11",
+  version: "0.19.11",
   default: [
     args:
-      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+      ~w(js/app.js --bundle --target=es2016,chrome58,edge109,firefox115,safari11 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
