727c7f354a65a181d0a4095458e08b40e9896ccb
Juan Morais <timrodz@icloud.com>
2025-08-20 23:24:51 +1200

    feat: Button variants

--- lib/mtg_friends_web/components/core_components.ex
+++ lib/mtg_friends_web/components/core_components.ex
@@ -246,12 +246,19 @@ defmodule MtgFriendsWeb.CoreComponents do
       <.button phx-click="go" variant="primary">Send!</.button>
       <.button navigate={~p"/"}>Home</.button>
   """
-  attr :rest, :global, include: ~w(href navigate patch)
-  attr :variant, :string, values: ~w(primary)
+  attr :rest, :global, include: ~w(href navigate patch disabled)
+  attr :variant, :string, values: ~w(primary secondary accent)
   slot :inner_block, required: true
 
   def button(%{rest: rest} = assigns) do
-    variants = %{"primary" => "btn-primary", nil => "btn-primary btn-soft"}
+    variants = %{
+      "primary" => "btn-primary",
+      "secondary" => "btn-secondary",
+      "accent" => "btn-accent",
+      "accent-outline" => "btn-accent btn-outline",
+      nil => "btn-primary btn-soft"
+    }
+
     assigns = assign(assigns, :class, Map.fetch!(variants, assigns[:variant]))
 
     if rest[:href] || rest[:navigate] || rest[:patch] do
