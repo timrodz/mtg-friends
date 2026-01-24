42b161420e689d66b721c925198e0a55dfaaae21
Juan Morais <timrodz@icloud.com>
2026-01-25 00:27:49 +1300

    chore: Install pre_commit

--- config/config.exs
+++ config/config.exs
@@ -61,3 +61,11 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
+
+config :pre_commit,
+  commands: [
+    "format",
+    "test --cover",
+    "dialyzer --format short"
+  ],
+  verbose: true
